a3c8e3fc0bf153e22d1990af0aeb1da5fe06e44c
Veleita <1veleita1@gmail.com>
2021-07-16 16:57:28 +0200

    greed_score.py tested

--- greed_score.py
+++ greed_score.py
@@ -11,12 +11,33 @@
 #   One   5   =>   50 point
 
 def score(dice):
-    score = 0
+    final_score = 0
+    score_table = {1: 0,
+                   2: 0,
+                   3: 0,
+                   4: 0,
+                   5: 0,
+                   6: 0}
 
     for value in dice:
-        print(value)
+        if value in score_table.keys():
+            score_table[value] += 1
 
-    return score
+    for key, value in score_table.items():
+        if key == 1:
+            if value >= 3:
+                score_table[key] = 1000 + (value - 3) * 100
+            elif value in range(1, 3):
+                score_table[key] = 100 * value
+        elif key == 5 and value in range(1, 3):
+            score_table[key] = 50 * value
+        elif value >= 3:
+            score_table[key] = key * 100
+        else:
+            score_table[key] = 0
+        final_score += score_table[key]
+
+    return final_score
 
 
 # TESTS
