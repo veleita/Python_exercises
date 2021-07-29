3b2f64829968b616a45055bed0b5dc3468e0158c
Veleita <1veleita1@gmail.com>
2021-07-29 10:49:12 +0200

    Added search and traverse functions for binary trees

--- binary_tree.py
+++ binary_tree.py
@@ -33,7 +33,7 @@ class BinaryTree:
     def __init__(self, root: Node):
         self.root = root
 
-    def inorder_traversal(self, root):
+    def inorder_traversal(self, root: Node):      # [left -> root -> right]
 
         data = []
         if root:
@@ -43,6 +43,43 @@ class BinaryTree:
 
         return data
 
+    def preorder_traversal(self, root: Node):      # [root -> left -> right]
+
+        data = []
+        if root:
+            data.append(root.data)
+            data += self.inorder_traversal(root.left)
+            data += self.inorder_traversal(root.right)
+
+        return data
+
+    def postorder_traversal(self, root:Node):      # [left -> right -> root]
+
+        data = []
+        if root:
+            data = self.inorder_traversal(root.left)
+            data += self.inorder_traversal(root.right)
+            data.append(root.data)
+
+        return data
+
+    def find_value(self, value, node=None):
+
+        if not node:
+            node = self.root
+
+        if value < node.data:
+            if node.left:
+                return self.find_value(value, node.left)
+            else:
+                return str(value) + " not found"
+        elif value > node.data:
+            if node.right:
+                return self.find_value(value, node.right)
+            else:
+                return str(value) + " not found"
+        else:
+            return str(value) + " found"
 
 # TESTS
 root = Node(12)
@@ -53,3 +90,8 @@ root.print_tree()
 
 tree = BinaryTree(root)
 print(tree.inorder_traversal(tree.root))
+print(tree.preorder_traversal(tree.root))
+print(tree.postorder_traversal(tree.root))
+
+print(tree.find_value(13))
+
