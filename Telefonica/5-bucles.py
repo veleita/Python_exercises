60bac8f2a6cdcd0c9dfe3d8d130bbd3d5feee1b6
Zome <maria.zomenogutierrez@telefonica.com>
2022-07-06 16:13:35 +0200

    Completed loop exercises

--- Telefonica/5-bucles.py
+++ Telefonica/5-bucles.py
@@ -9,8 +9,18 @@ def decompose_number(num):
             prime_num += 1
     if num > 1:
         prime_factors.append(num)
-    print(prime_factors)
+    return prime_factors
 
-decompose_number(18)
-decompose_number(21)
-decompose_number(78675)
+print(decompose_number(18))
+print(decompose_number(21))
+print(decompose_number(78675))
+
+
+def greatest_common_divisor(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+print(greatest_common_divisor(20, 15))
+print(greatest_common_divisor(245, 1560))
+print(greatest_common_divisor(21, 7))
