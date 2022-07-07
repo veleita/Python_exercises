af156b88247ed10d810322c5e75d26ba9fb3f67c
Zome <maria.zomenogutierrez@telefonica.com>
2022-07-07 10:33:56 +0200

    Completed exceptions exercise

--- Telefonica/7-excepciones.py
+++ Telefonica/7-excepciones.py
@@ -8,8 +8,34 @@ def add_element(lst, new_el):
         lst.append(new_el)
 
 nums = [2, 3, 4, 5]
-add_element(nums, 1)
-print(nums)
-add_element(nums, 5)
-print(nums)
+try:
+    add_element(nums, 1)
+    print(nums)
+except ValueError as RepeatedElement:
+    print(RepeatedElement)
+try:
+    add_element(nums, 2)
+    print(nums)
+except ValueError as RepeatedElement:
+    print(RepeatedElement)
 
+
+def get_value_from_key(dict, key):
+    if key not in dict:
+        raise ValueError("The provided key doesn't exist in this dictionary")
+    else:
+        return dict[key]
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+try:
+    print(get_value_from_key(car, "year"))
+except ValueError as KeyNotFound:
+    print(KeyNotFound)
+try:
+    print(get_value_from_key(car, "yea"))
+except ValueError as KeyNotFound:
+    print(KeyNotFound)
